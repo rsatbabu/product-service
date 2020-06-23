8bd2dd98656c1bda27b32dc8e3024248a91d3d4a
rblbabu@gmail.com <rblbabu@gmail.com>
2020-06-23 15:20:36 -0400

    Remove unused imports

--- src/main/java/com/ppe/productservice/product/controller/ProductController.java
+++ src/main/java/com/ppe/productservice/product/controller/ProductController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
