5232fa3dbc69163b854bfcdc6238790c7c604ffb
rblbabu@gmail.com <rblbabu@gmail.com>
2020-06-22 23:05:08 -0400

    updated the description for gloves

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(3,'
 
 
 INSERT INTO productmongo(id,detail)VALUES(1,'{"id":1,"name":"Gloves","description":"Quantity in 1 Box - 100 Gloves = 50 pairs in 1 box","price":50,"instock":100}');
-INSERT INTO productmongo(id,detail)VALUES(2,'{"id":2,"name":"Surgical Face Mask","description":"Ships from Canada - 100 Pack Disposable Face Safety, 3-Ply Ear Loop - Perfect for avoiding contagion in public areas..","price":40,"instock":100}');
+INSERT INTO productmongo(id,detail)VALUES(2,'{"id":2,"name":"Surgical Face Mask","description":"Ships from Canada - 100 Pack Disposable Face Safety, 3-Ply Ear Loop - Perfect for avoiding contagion in public areas.","price":40,"instock":100}');
 INSERT INTO productmongo(id,detail)VALUES(3,'{"id":3,"name":"Hand Sanitizers","description":"Good hand hygiene kills transient microorganisms and removal of visible soil. Using an alcohol-based hand rub or soap and running water helps accomplish the task.","price":10,"instock":100}');
 
 INSERT INTO customer(customer_id, email,phone)VALUES('john','test@email.com','514-111-1111');
