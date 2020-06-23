eed34ec2b277b2c6115292d449ab694dd44d7e75
rblbabu@gmail.com <rblbabu@gmail.com>
2020-06-22 23:03:58 -0400

    updated the initial stock for Gloves product in db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 
-INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(1,'Gloves',50,12,0,10,'Quantity in 1 Box - 100 Gloves = 50 pairs in 1 box.');
+INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(1,'Gloves',50,12,0,100,'Quantity in 1 Box - 100 Gloves = 50 pairs in 1 box.');
 INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(2,'Surgical Face Mask',40,100,10,100,'Ships from Canada - 100 Pack Disposable Face Safety, 3-Ply Ear Loop - Perfect for avoiding contagion in public areas.');
 INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(3,'Hand Sanitizers',10,100,0,10,'Good hand hygiene kills transient microorganisms and removal of visible soil. Using an alcohol-based hand rub or soap and running water helps accomplish the task.');
 
@@ -8,7 +8,7 @@ INSERT INTO product(id,name,price,instock,inorder,reorder,description)VALUES(3,'
 
 
 
-INSERT INTO productmongo(id,detail)VALUES(1,'{"id":1,"name":"Gloves","description":"Quantity in 1 Box - 100 Gloves = 50 pairs in 1 box","price":50,"instock":12}');
+INSERT INTO productmongo(id,detail)VALUES(1,'{"id":1,"name":"Gloves","description":"Quantity in 1 Box - 100 Gloves = 50 pairs in 1 box","price":50,"instock":100}');
 INSERT INTO productmongo(id,detail)VALUES(2,'{"id":2,"name":"Surgical Face Mask","description":"Ships from Canada - 100 Pack Disposable Face Safety, 3-Ply Ear Loop - Perfect for avoiding contagion in public areas..","price":40,"instock":100}');
 INSERT INTO productmongo(id,detail)VALUES(3,'{"id":3,"name":"Hand Sanitizers","description":"Good hand hygiene kills transient microorganisms and removal of visible soil. Using an alcohol-based hand rub or soap and running water helps accomplish the task.","price":10,"instock":100}');
 
